c2fe9605654fd453814c8f33b111cf24fc38e80c
Abhinandan Madaan <abhimadaan96@gmail.com>
2024-07-24 15:24:44 +0530

    Latest UI changes in sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,14 +1,27 @@
 
 self.addEventListener('push', function(event) {
-    console.log('[Service Worker] Push Received.');
+    console.log('[Service Worker] Push Received...');
     console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+    
     const pushData = event.data.json(); // Parse the data from the push event as JSON
 
     const title = pushData.notification.title;
     const options = {
         body: pushData.notification.body,
-        icon: 'images/icon.png',
-        badge: 'images/badge.png'
+        actions: pushData.notification.actions,
+        icon: pushData.notification.icon,
+        badge: pushData.notification.badge,
+        image: pushData.notification.image,
+        data: pushData.notification.data,
+        dir: pushData.notification.direction,
+        lang: pushData.notification.language,
+        renotify: pushData.notification.renotify,
+        requireInteraction: pushData.notification.requireInteraction,
+        tag: pushData.notification.tag,
+        timestamp: pushData.notification.timestampMillis,
+        vibrate: pushData.notification.vibrate,
+        customData: pushData.notification.customData,
+        silent: pushData.notification.silent
     };
 
     event.waitUntil(self.registration.showNotification(title, options));
